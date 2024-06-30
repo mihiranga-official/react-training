96b0d3e2cbcc100c461274b10aa6996e5dce8714
mihiranga-official <mihiranga.office@gmail.com>
2024-06-30 19:43:06 +0530

    JSX FInish

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,29 @@ function App() {
   const getFullName = (f, m, s) => {
     return `${f} ${m} ${s}`;
   };
-  const arr=["Apple","Grapes","Oranages"]
+  const arr = ["Apple", "Grapes", "Oranages"];
+  const lang = (
+    <ul>
+      <li>React </li>
+      <li>Vue</li>
+      <li>Vanila</li>
+      <li>Java</li>
+    </ul>
+  );
+  const objects={
+    fname:"Shashika",
+    age:"23"
+  }
   return (
     <>
       <div className="h1">
-        <h1>Hi My Name is {getFullName("Janith", "Mihiranga", "Gunawardana")}</h1>
+        <h1>
+          Hi My Name is {getFullName("Janith", "Mihiranga", "Gunawardana")}
+        </h1>
         <p>I would like to eat {arr[1]}</p>
+        <p>Good Langauages are </p>
+        <span>{lang}</span>
+        <h2>Her name is {objects.fname} and her age is {objects.age} </h2>
       </div>
     </>
   );
